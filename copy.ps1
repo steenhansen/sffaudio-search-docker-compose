e2351c668778d4e3b85efcfe8104f6f1c16070b5
steen hansen <steen_hansen@yahoo.com>
2018-07-16 23:05:23 -0700

    ...
    ...

--- copy.ps1
+++ copy.ps1
@@ -8,7 +8,7 @@ echo 'AAAAAAAAAAAAAAAAAA'
 echo $systemTeamcityBuildCheckoutDir 
 
 $sourceRoot = "D:\github\podcast_neo4j_conf\podcast-neo4j\node_modules"
-$destinationRoot = $systemTeamcityBuildCheckoutDir + "\node_modules"
+$destinationRoot = $systemTeamcityBuildCheckoutDir 
 echo $sourceRoot 
 echo $destinationRoot 
 
