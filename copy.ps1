8e8ccafd3993911572770605922cb584d67c920c
steen hansen <steen_hansen@yahoo.com>
2018-07-16 23:07:46 -0700

    ...
    ...

--- copy.ps1
+++ copy.ps1
@@ -8,7 +8,7 @@ echo 'AAAAAAAAAAAAAAAAAA'
 echo $systemTeamcityBuildCheckoutDir 
 
 $sourceRoot = "D:\github\podcast_neo4j_conf\podcast-neo4j\node_modules"
-$destinationRoot = $systemTeamcityBuildCheckoutDir 
+$destinationRoot = $systemTeamcityBuildCheckoutDir + "\node_modules"
 echo $sourceRoot 
 echo $destinationRoot 
 
@@ -18,7 +18,8 @@ echo 'BBBBBBBBBBBBBBBBBBBBB'
 
 
 if(!(Test-Path -Path $destinationRoot )){
-    New-Item -ItemType directory -Path $destinationRoot
+    ##New-Item -ItemType directory -Path $destinationRoot
+    mkdir $destinationRoot
     Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
 }
 
