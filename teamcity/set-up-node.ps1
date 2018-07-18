362fc2f524dba6c3c352dd31a9a2d9bde4a00d0d
steen hansen <steen_hansen@yahoo.com>
2018-07-18 16:14:56 -0700

    test
    test

--- teamcity/set-up-node.ps1
+++ teamcity/set-up-node.ps1
@@ -1,3 +1,4 @@
+echo 'set-up-node.ps1 start'
 
 $teamcity_dir = (Get-Item -Path ".\").FullName
 $podcast_neo4j_dir = (Get-Item -Path ".\").parent.FullName
@@ -10,12 +11,12 @@ $exist_node_modules_config = Test-Path -Path $node_modules_config
 	$podcast_neo4j_node_modules =  $podcast_neo4j_dir +'/node_modules/'
 
 if( $exist_node_modules_config ){
+	echo 'set-up-node.ps1 exists'
 	$config_node_modules_wild = $node_modules_config + '*'
-
 	mkdir $podcast_neo4j_node_modules
     Copy-Item -Path $config_node_modules_wild -Recurse -Destination $podcast_neo4j_node_modules -Container
-
 } else {
+	echo 'set-up-node.ps1 NOT exists'
 	$build_node_modules = Start-Job -FilePath './build-node-modules.ps1' -ArgumentList $podcast_neo4j_dir
 	Wait-Job $build_node_modules
 	Receive-Job $build_node_modules
@@ -24,5 +25,5 @@ if( $exist_node_modules_config ){
     mkdir $node_modules_config
 	Copy-Item -Path $node_modules_wild -Recurse -Destination $node_modules_config -Container
 }
-
+echo 'set-up-node.ps1 done'
 
