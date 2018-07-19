04a6d493ded632ad2be2d3402ffc73331d86af65
steen hansen <steen_hansen@yahoo.com>
2018-07-19 00:24:07 -0700

    ...

---
+++ build-powershell/build-all.ps1
@@ -0,0 +1,12 @@
+
+
+
+
+# N.B. This file must be run from podcast-neo4j directory. NOT build-powershell directory
+
+#  /podcast-neo4j/>                  .\build-powershell/cli-build.ps1
+
+
+
+.\build-powershell/set-up-node.ps1
+.\build-powershell/mocha-test.ps1
--- build-powershell/set-up-node.ps1
+++ build-powershell/set-up-node.ps1
@@ -1,15 +1,19 @@
 
 
 
+# build_powershell
+
+
+
 
 # say what is being built where so it can make sense
 
 
 
-# N.B. This file must be run from podcast-neo4j directory. NOT teamcity directory
+# N.B. This file must be run from podcast-neo4j directory. NOT build-powershell directory
 
 
-#  /podcast-neo4j/>                  .\teamcity/set-up-node.ps1
+#  /podcast-neo4j/>                  .\build-powershell/set-up-node.ps1
 
 
 
@@ -18,7 +22,7 @@ $podcast_neo4j_node_modules =  $podcast_neo4j_dir +'/node_modules/'
 $exist_node_modules = Test-Path -Path $podcast_neo4j_node_modules
 
 if( ! $exist_node_modules ){
-	$build_node_modules = Start-Job -FilePath './teamcity/build-node-modules.ps1' -ArgumentList $podcast_neo4j_dir
+	$build_node_modules = Start-Job -FilePath './build-powershell/build-node-modules.ps1' -ArgumentList $podcast_neo4j_dir
 	Wait-Job $build_node_modules
 	Receive-Job $build_node_modules
 }
