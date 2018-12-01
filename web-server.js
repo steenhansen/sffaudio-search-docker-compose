0866c87daecee812fc0d8b3938cf49bf2d12c08c
steen hansen <steen_hansen@yahoo.com>
2018-11-30 23:51:32 -0800

    match old heroku profile

---
+++ web-server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+  node server-heroku
+ */
+
+require('./sff-network/global-require');
+const heroku_environment = rootAppRequire('sff-network/check-start/heroku-environment');
+heroku_environment.processEnvVars();
+
+require('./sff-network/check-start/web-server');
+
+
+
+
+
