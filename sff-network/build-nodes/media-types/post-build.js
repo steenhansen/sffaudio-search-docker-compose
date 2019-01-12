55308e06c8c08b1c9d49e82350e29d1d45d9292e
steen hansen <steen_hansen@yahoo.com>
2019-01-11 18:11:22 -0800

    crash fix

--- sff-network/build-nodes/media-types/post-build.js
+++ sff-network/build-nodes/media-types/post-build.js
@@ -33,36 +33,36 @@ module.exports = function (build_repository) {
             var book_build = rootAppRequire('./sff-network/build-nodes/media-types/book-build')(build_repository);
             var post_promises = [];
             for (let book_author of author_book_obj) {
-                var graph_title = book_author['title'];
-                var author = book_author['author'];
-                var book = book_author['book'];
-                var sff_post_url = book_author['id'];
-                var under_title = misc_helper.alphaUnderscore(book);
-                if (under_title) {
-                    var author_obj = {};
-                    var multiple_monikers = new MultipleMonikers();
-                    var last_first_underscores = multiple_monikers.lastUnderscore();
-                    var firstMiddleLast = multiple_monikers.firstMiddleLast();
-                    for (var an_author of firstMiddleLast) {
-                        var strip_author = misc_helper.alphaUnderscore(an_author);
-                        author_obj[strip_author] = an_author;
-                        var author_nodes = author_build.addAuthors(author_obj);
-                        post_promises.push(author_nodes);
-                    }
-                    var title_with_authors = multiple_monikers.titleWithAuthors(under_title);
-                    var book_obj = {
-                        sorted_label: under_title,
-                        esc_book_title: book,
-                        under_title: under_title,
-                        last_first_underscores: last_first_underscores
-                    };
-                    var ojbect_of_book = {};
-                    ojbect_of_book[title_with_authors] = book_obj;
-                    var book_nodes = book_build.addBooksNew(ojbect_of_book);
-                    post_promises.push(book_nodes);
-                    var post_promise = build_repository.saveBookPost(strip_author, under_title, sff_post_url, graph_title, graph_title);
-                    post_promises.push(post_promise);
-                }
+                // var graph_title = book_author['title'];
+                // var author = book_author['author'];
+                // var book = book_author['book'];
+                // var sff_post_url = book_author['id'];
+                // var under_title = misc_helper.alphaUnderscore(book);
+                // if (under_title) {
+                //     var author_obj = {};
+                //     var multiple_monikers = new MultipleMonikers();
+                //     var last_first_underscores = multiple_monikers.lastUnderscore();
+                //     var firstMiddleLast = multiple_monikers.firstMiddleLast();
+                //     for (var an_author of firstMiddleLast) {            //firstMiddleLast is not iterable
+                //         var strip_author = misc_helper.alphaUnderscore(an_author);
+                //         author_obj[strip_author] = an_author;
+                //         var author_nodes = author_build.addAuthors(author_obj);
+                //         post_promises.push(author_nodes);
+                //     }
+                //     var title_with_authors = multiple_monikers.titleWithAuthors(under_title);
+                //     var book_obj = {
+                //         sorted_label: under_title,
+                //         esc_book_title: book,
+                //         under_title: under_title,
+                //         last_first_underscores: last_first_underscores
+                //     };
+                //     var ojbect_of_book = {};
+                //     ojbect_of_book[title_with_authors] = book_obj;
+                //     var book_nodes = book_build.addBooksNew(ojbect_of_book);
+                //     post_promises.push(book_nodes);
+                //     var post_promise = build_repository.saveBookPost(strip_author, under_title, sff_post_url, graph_title, graph_title);
+                //     post_promises.push(post_promise);
+                // }
             }
             return post_promises;
         }
